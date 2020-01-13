7df3efbd61dcdf1abe9af84ce91fb67c6bef71ca
Shayan Khan <shayanaijaz@gmail.com>
2020-01-13 13:19:08 -0600

    test

---
+++ Test_Login/Test_Login/Test_Login.Android/obj/Debug/90/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 080a56c9c2dcc63028d817ca8e32b1a8a453fcc5 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1421
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 372303
+	.include	"typemap.mj.inc"
---
+++ Test_Login/Test_Login/Test_Login/MainPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test_Login
+{
+    class MainPageViewModel
+    {
+    }
+}
